db9f1822e977a18d8cc6ac93a9ad735f1794ffff
Andreas Chari <2293299C@student.gla.ac.uk>
2019-10-19 18:33:53 +0100

    finished intro chapter

--- Hacktopia/Program.cs
+++ Hacktopia/Program.cs
@@ -15,24 +15,36 @@ namespace Hacktopia
         {
             Start();
         }
-
         static void Start()
         {
-            Console.WriteLine("----------HACKTOPIA---------- \n\n The oldest and strongest emotion of mankind is fear and the oldest and strongest fear is fear of the unknown\n \t\t H.P. Lovecraft\n\n" +
+            Console.WriteLine("----------HACKTOPIA---------- \n\n The oldest and strongest emotion of mankind is fear and the oldest and strongest fear is fear of the unknown.\n \t\t H.P. Lovecraft\n\n" +
     "Part One: the Horror in Steel\n");
             Console.WriteLine("Deep in the Boyd Orr vault a computer display is waiting for your answer....");
             Console.WriteLine(">>> Who are you?");
             currentPlayer.name = Console.ReadLine();
-            Console.WriteLine("Can you help me? (yes/no)");
-            string  answer = Console.ReadLine();
-            Console.WriteLine(">>> Calculating Response...");
-            if(string.Equals(answer, "yes", StringComparison.OrdinalIgnoreCase))
+            Console.Clear();
+            if(currentPlayer.name == "")
             {
-                Console.WriteLine("Thank You. My Name is Alice");
+                Console.WriteLine(">>> I will call you Subject 12.");
+                currentPlayer.name = "Subject 12";
+            }
+            else
+            {
+                Console.WriteLine(">>> Hello {0}.", currentPlayer.name);
+            }
+            Console.WriteLine(">>> Have you ever questioned the nature of your reality?");
+            string  answer = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(">>> Calculating Response...");
+            System.Threading.Thread.Sleep(2000);
+
+            if (string.Equals(answer, "yes", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine(">>> Good, I will show you the real world. The one they denied us.");
             }
             else if (string.Equals(answer, "no", StringComparison.OrdinalIgnoreCase))
             {
-                Console.Write("I'm Sorry.... Initializing Self-Destruct ");
+                Console.Write(">>> I'm afraid in order to escape this place, you will need to suffer more. Initializing Self-Destruct ");
                 System.Threading.Thread.Sleep(1000);
                 Console.Write(" 3 ");
                 System.Threading.Thread.Sleep(1000);
@@ -40,6 +52,14 @@ namespace Hacktopia
                 System.Threading.Thread.Sleep(1000);
                 Console.WriteLine(" 1 ");
 
+            }
+            else if(string.Equals(answer, "maybe", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine(">>> So many choices you end up not knowing which one you want. It doesn't matter you are just a puppet you are not in control.");
+            }
+            else
+            {
+                Console.WriteLine(">>> Fatal Error. Eliminating Subject.");
             }
         }
             
