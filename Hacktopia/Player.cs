ec113fedd0618c51c457aa82d6189394dec2b7a8
Andreas Chari <2293299C@student.gla.ac.uk>
2019-10-19 18:09:06 +0100

    Added intro and basic player functionality

--- Hacktopia/Player.cs
+++ Hacktopia/Player.cs
@@ -6,6 +6,9 @@ namespace Hacktopia
 {
     class Player
     {
-
+        public string name;
+        public int health = 10;
+        public int sanity = 10;
+        public int score = 0;
     }
 }
--- Hacktopia/Program.cs
+++ Hacktopia/Program.cs
@@ -3,19 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Channels;
 
 namespace Hacktopia
 {
     class Program
     {
-         private static void prelude()
-        {
-            Console.WriteLine("----HACKTOPIA: CHAPTER 1----\n"+
-                "12 Months ago the world ended... 6 brave scientists " +
-                "travelled deep into the ruins of the Boyd Orr building \n to find the last working" +
-                "electronic devices on earth. \n All expect one were running MyCampus and were useless.\n One was still operational, display a single question");
-        }
-
+        public static Player currentPlayer = new Player();
+   
         static void Main(string[] args)
         {
             Start();
@@ -23,10 +18,29 @@ namespace Hacktopia
 
         static void Start()
         {
-            prelude();
-            Console.WriteLine("Enter Player Name: ");
-            string playerName = Console.ReadLine();
+            Console.WriteLine("----------HACKTOPIA---------- \n\n The oldest and strongest emotion of mankind is fear and the oldest and strongest fear is fear of the unknown\n \t\t H.P. Lovecraft\n\n" +
+    "Part One: the Horror in Steel\n");
+            Console.WriteLine("Deep in the Boyd Orr vault a computer display is waiting for your answer....");
+            Console.WriteLine(">>> Who are you?");
+            currentPlayer.name = Console.ReadLine();
+            Console.WriteLine("Can you help me? (yes/no)");
+            string  answer = Console.ReadLine();
+            Console.WriteLine(">>> Calculating Response...");
+            if(string.Equals(answer, "yes", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Thank You. My Name is Alice");
+            }
+            else if (string.Equals(answer, "no", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.Write("I'm Sorry.... Initializing Self-Destruct ");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write(" 3 ");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write(" 2 ");
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine(" 1 ");
 
+            }
         }
             
         
