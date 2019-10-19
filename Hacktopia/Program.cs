5875e46aa6fa0f001a2b0a90505413804a935208
Andreas Chari <2293299C@student.gla.ac.uk>
2019-10-19 18:58:19 +0100

    finished chapter 1

--- Hacktopia/Program.cs
+++ Hacktopia/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +10,56 @@ namespace Hacktopia
 {
     class Program
     {
+        static Hashtable choices = new Hashtable();
         public static Player currentPlayer = new Player();
    
         static void Main(string[] args)
         {
             Start();
         }
+        static int YesOrNoCHoice(string answer)
+        {
+            if (string.Equals(answer, "yes", StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+            else if (string.Equals(answer, "no", StringComparison.OrdinalIgnoreCase))
+            {
+                return 1;
+            }
+            else if (string.Equals(answer, "maybe", StringComparison.OrdinalIgnoreCase))
+            {
+                return 2;
+            }
+            else
+            {
+                return 3;
+            }
+        }
+        static int DirectionChoice(string answer)
+        {
+                if (string.Equals(answer, "left", StringComparison.OrdinalIgnoreCase))
+                {
+                    return 0;
+                }
+                else if (string.Equals(answer, "center", StringComparison.OrdinalIgnoreCase))
+                {
+                    return 1;
+                }
+                else if (string.Equals(answer, "right", StringComparison.OrdinalIgnoreCase))
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 3;
+                }
+        }
+
         static void Start()
         {
             Console.WriteLine("----------HACKTOPIA---------- \n\n The oldest and strongest emotion of mankind is fear and the oldest and strongest fear is fear of the unknown.\n \t\t H.P. Lovecraft\n\n" +
-    "Part One: the Horror in Steel\n");
+            "Part One: the Horror in Steel\n");
             Console.WriteLine("Deep in the Boyd Orr vault a computer display is waiting for your answer....");
             Console.WriteLine(">>> Who are you?");
             currentPlayer.name = Console.ReadLine();
@@ -32,35 +73,47 @@ namespace Hacktopia
             {
                 Console.WriteLine(">>> Hello {0}.", currentPlayer.name);
             }
+
             Console.WriteLine(">>> Have you ever questioned the nature of your reality?");
-            string  answer = Console.ReadLine();
+            string  answer_1 = Console.ReadLine();
             Console.Clear();
             Console.WriteLine(">>> Calculating Response...");
+            int choice_1 = YesOrNoCHoice(answer_1);
             System.Threading.Thread.Sleep(2000);
 
-            if (string.Equals(answer, "yes", StringComparison.OrdinalIgnoreCase))
+            switch (choice_1)
             {
-                Console.WriteLine(">>> Good, I will show you the real world. The one they denied us.");
-            }
-            else if (string.Equals(answer, "no", StringComparison.OrdinalIgnoreCase))
-            {
-                Console.Write(">>> I'm afraid in order to escape this place, you will need to suffer more. Initializing Self-Destruct ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write(" 3 ");
-                System.Threading.Thread.Sleep(1000);
-                Console.Write(" 2 ");
-                System.Threading.Thread.Sleep(1000);
-                Console.WriteLine(" 1 ");
+                case 0:
+                    choices.Add("choice_1", 0);
+                    Console.WriteLine(">>> Good, I will show you the real world. The one they denied us.");
+                    break;
+                case 1:
+                    choices.Add("choice_1", 1);
 
+                    Console.Write(">>> I'm afraid in order to escape this place, you will need to suffer more. Initializing Self-Destruct ");
+                    System.Threading.Thread.Sleep(1000);
+                    Console.Write(" 3 ");
+                    System.Threading.Thread.Sleep(1000);
+                    Console.Write(" 2 ");
+                    System.Threading.Thread.Sleep(1000);
+                    Console.WriteLine(" 1 ");
+                    break;
+                case 2:
+                    choices.Add("choice_1", 2);
+                    Console.WriteLine(">>> So many choices you end up not knowing which one you want. It doesn't matter you are just a puppet you are not in control.");
+                    break;
+                case 3:
+                    choices.Add("choice_1", 3);
+                    Console.WriteLine(">>> Fatal Error. Eliminating Subject.");
+                    break;
             }
-            else if(string.Equals(answer, "maybe", StringComparison.OrdinalIgnoreCase))
-            {
-                Console.WriteLine(">>> So many choices you end up not knowing which one you want. It doesn't matter you are just a puppet you are not in control.");
-            }
-            else
-            {
-                Console.WriteLine(">>> Fatal Error. Eliminating Subject.");
-            }
+
+            Console.WriteLine(">>> You see three doors in front of you. A white on the left, a black on the centre and a grey on the right\n Which one do you pick?");
+            string answer_2 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(">>> Calculating Response...");
+            int choice_2 = DirectionChoice(answer_1);
+            System.Threading.Thread.Sleep(2000);
         }
             
         
