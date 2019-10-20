c255461d803a8f79808db39faf9c2cb034f92a14
Andreas Chari <2293299C@student.gla.ac.uk>
2019-10-20 09:42:02 +0100

    added getters setters to Player class

--- Hacktopia/Player.cs
+++ Hacktopia/Player.cs
@@ -11,5 +11,10 @@ namespace Hacktopia
         public int sanity = 10;
         public int score = 0;
 
+        public int Health { get; set; }
+        public string Name { get; set; }
+        public int Sanity { get; set; }
+        public int Score { get; set; }
+
     }
 }
